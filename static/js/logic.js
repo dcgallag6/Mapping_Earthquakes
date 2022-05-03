128500221fd19b979ba6bb7028208490873acb98
Daniel Gallagher <dcgallag6@gmail.com>
2022-05-03 15:47:02 -0700

    .env init

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,6 @@
 // create the tile layer that will be the background of the map.
+import 'dotenv/config'
+require('dotenv').config()
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
